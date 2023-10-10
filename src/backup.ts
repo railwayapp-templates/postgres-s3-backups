d71a18420bebd188fdde6b8cff451077d073dd27
drmarshall <6020152+drmarshall@users.noreply.github.com>
2023-10-10 10:51:18 -0700

    S3 -> GCS

--- src/backup.ts
+++ src/backup.ts
@@ -1,35 +1,24 @@
 import { exec } from "child_process";
-import { PutObjectCommand, S3Client, S3ClientConfig } from "@aws-sdk/client-s3";
-import { createReadStream, unlink } from "fs";
+import { Storage, UploadOptions } from "@google-cloud/storage";
+import { unlink } from "fs";
 
 import { env } from "./env";
 
-const uploadToS3 = async ({ name, path }: {name: string, path: string}) => {
-  console.log("Uploading backup to S3...");
+const uploadToGCS = async ({ name, path }: { name: string; path: string }) => {
+  console.log("Uploading backup to GCS...");
 
-  const bucket = env.AWS_S3_BUCKET;
+  const bucketName = env.GCS_BUCKET;
 
-  const clientOptions: S3ClientConfig = {
-    region: env.AWS_S3_REGION,
-  }
+  const uploadOptions: UploadOptions = {
+    destination: name,
+  };
 
-  if (env.AWS_S3_ENDPOINT) {
-    console.log(`Using custom endpoint: ${env.AWS_S3_ENDPOINT}`)
-    clientOptions['endpoint'] = env.AWS_S3_ENDPOINT;
-  }
+  const storage = new Storage();
 
-  const client = new S3Client(clientOptions);
+  await storage.bucket(bucketName).upload(path, uploadOptions);
 
-  await client.send(
-    new PutObjectCommand({
-      Bucket: bucket,
-      Key: name,
-      Body: createReadStream(path),
-    })
-  )
-
-  console.log("Backup uploaded to S3...");
-}
+  console.log("Backup uploaded to GCS...");
+};
 
 const dumpToFile = async (path: string) => {
   console.log("Dumping DB to file...");
@@ -37,7 +26,7 @@ const dumpToFile = async (path: string) => {
   await new Promise((resolve, reject) => {
     exec(
       `pg_dump ${env.BACKUP_DATABASE_URL} -F t | gzip > ${path}`,
-      (error, stdout, stderr) => {
+      (error, _, stderr) => {
         if (error) {
           reject({ error: JSON.stringify(error), stderr });
           return;
@@ -49,7 +38,7 @@ const dumpToFile = async (path: string) => {
   });
 
   console.log("DB dumped to file...");
-}
+};
 
 const deleteFile = async (path: string) => {
   console.log("Deleting file...");
@@ -59,20 +48,20 @@ const deleteFile = async (path: string) => {
       return;
     });
     resolve(undefined);
-  })
-}
+  });
+};
 
 export const backup = async () => {
-  console.log("Initiating DB backup...")
+  console.log("Initiating DB backup...");
 
-  let date = new Date().toISOString()
-  const timestamp = date.replace(/[:.]+/g, '-')
-  const filename = `backup-${timestamp}.tar.gz`
-  const filepath = `/tmp/${filename}`
+  let date = new Date().toISOString();
+  const timestamp = date.replace(/[:.]+/g, "-");
+  const filename = `${env.BACKUP_PREFIX}backup-${timestamp}.tar.gz`;
+  const filepath = `/tmp/${filename}`;
 
-  await dumpToFile(filepath)
-  await uploadToS3({name: filename, path: filepath})
-  await deleteFile(filepath)
+  await dumpToFile(filepath);
+  await uploadToGCS({ name: filename, path: filepath });
+  await deleteFile(filepath);
 
-  console.log("DB backup complete...")
-}
+  console.log("DB backup complete...");
+};
