709ceb53d93b88c88d1dbf739f171c29f044e82b
pieeee <hello@ahnaf.io>
2024-09-10 07:13:00 +0600

    feat: backup retention

--- src/env.ts
+++ src/env.ts
@@ -6,55 +6,55 @@ export const env = envsafe({
   AWS_S3_BUCKET: str(),
   AWS_S3_REGION: str(),
   BACKUP_DATABASE_URL: str({
-    desc: "The connection string of the database to backup.",
+    desc: 'The connection string of the database to backup.'
   }),
   BACKUP_CRON_SCHEDULE: str({
-    desc: "The cron schedule to run the backup on.",
-    default: "0 5 * * *",
-    allowEmpty: true,
+    desc: 'The cron schedule to run the backup on.',
+    default: '0 5 * * *',
+    allowEmpty: true
   }),
   AWS_S3_ENDPOINT: str({
-    desc: "The S3 custom endpoint you want to use.",
-    default: "",
+    desc: 'The S3 custom endpoint you want to use.',
+    default: '',
     allowEmpty: true,
   }),
   AWS_S3_FORCE_PATH_STYLE: bool({
-    desc: "Use path style for the endpoint instead of the default subdomain style, useful for MinIO",
+    desc: 'Use path style for the endpoint instead of the default subdomain style, useful for MinIO',
     default: false,
-    allowEmpty: true,
+    allowEmpty: true
   }),
   RUN_ON_STARTUP: bool({
-    desc: "Run a backup on startup of this application",
+    desc: 'Run a backup on startup of this application',
     default: false,
     allowEmpty: true,
   }),
   BACKUP_FILE_PREFIX: str({
-    desc: "Prefix to the file name",
-    default: "backup",
+    desc: 'Prefix to the file name',
+    default: 'backup',
   }),
   BUCKET_SUBFOLDER: str({
-    desc: "A subfolder to place the backup files in",
-    default: "",
-    allowEmpty: true,
+    desc: 'A subfolder to place the backup files in',
+    default: '',
+    allowEmpty: true
   }),
   SINGLE_SHOT_MODE: bool({
-    desc: "Run a single backup on start and exit when completed",
+    desc: 'Run a single backup on start and exit when completed',
     default: false,
     allowEmpty: true,
   }),
   // This is both time consuming and resource intensive so we leave it disabled by default
   SUPPORT_OBJECT_LOCK: bool({
-    desc: "Enables support for buckets with object lock by providing an MD5 hash with the backup file",
-    default: false,
+    desc: 'Enables support for buckets with object lock by providing an MD5 hash with the backup file',
+    default: false
   }),
   BACKUP_OPTIONS: str({
-    desc: "Any valid pg_dump option.",
-    default: "",
+    desc: 'Any valid pg_dump option.',
+    default: '',
     allowEmpty: true,
   }),
   RETENTION_DAYS: num({
     desc: "Number of days to retain backups. If not set, cleanup is disabled.",
     default: undefined,
-    allowEmpty: true
+    allowEmpty: true,
   }),
-});
+})
